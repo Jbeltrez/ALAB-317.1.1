9f3d197c4e465473af150c5f1e34543358662b9b
John Beltrez <65853308+Jbeltrez@users.noreply.github.com>
2024-11-06 16:19:34 +0000

    parts 2 and 3 finished and code properly adjusted

--- src/index.ts
+++ src/index.ts
@@ -1,35 +1,41 @@
-//  @ts-check
-class Vehicle {
-  status = "stopped";
+// @ts-check
 
-  constructor(make, model, wheels) {
+class Vehicle {
+  make: string;
+  model: string;
+  wheels: number;
+  status: "started" | "stopped" = "stopped";
+
+  constructor(make: string, model: string, wheels: number) {
     this.make = make;
     this.model = model;
     this.wheels = wheels;
   }
+
   start() {
     this.status = "started";
   }
+
   stop() {
     this.status = "stopped";
   }
 }
 
 class Car extends Vehicle {
-  constructor(make, model) {
-    super(make, model, "four");
+  constructor(make: string, model: string) {
+    super(make, model, 4);
   }
 }
 
 class MotorCycle extends Vehicle {
-  constructor(make, model) {
+  constructor(make: string, model: string) {
     super(make, model, 2);
   }
 }
 
-function printStatus(vehicle) {
-  if (vehicle.status === "running") {
-    console.log("The vehicle is running.");
+function printStatus(vehicle: Vehicle) {
+  if (vehicle.status === "started") {
+    console.log("The vehicle is started.");
   } else {
     console.log("The vehicle is stopped.");
   }
@@ -38,9 +44,99 @@ function printStatus(vehicle) {
 const myHarley = new MotorCycle("Harley-Davidson", "Low Rider S");
 myHarley.start();
 printStatus(myHarley);
-console.log(myHarley.make.toUppercase());
+console.log(myHarley.make.toUpperCase());
 
 const myBuick = new Car("Buick", "Regal");
 myBuick.wheels = myBuick.wheels - 1;
 console.log(myBuick.wheels);
-console.log(myBuick.mdl);
+console.log(myBuick.model);
+
+class NCycle<T> {
+  status: "started" | "stopped" = "stopped";
+  make: T | T[];
+  model: T | T[];
+
+  constructor(make: T | T[], model: T | T[]) {
+    this.make = make;
+    this.model = model;
+  }
+
+  start(): void {
+    this.status = "started";
+  }
+
+  stop(): void {
+    this.status = "stopped";
+  }
+
+  private areBothArrays(): boolean {
+    // No need for parameters because the context is this
+    return Array.isArray(this.make) && Array.isArray(this.model);
+  }
+
+  private areArraysMatchingLength(): boolean {
+    return (
+      this.isArray(this.make) &&
+      this.isArray(this.model) &&
+      this.make.length === this.model.length
+    );
+  }
+
+  private printPair(make: T, model: T, index?: number): void {
+    if (index !== undefined) {
+      console.log(`This NCycle has a ${make} ${model} at ${index}.`);
+    } else {
+      console.log(`This is a ${make} ${model} NCycle.`);
+    }
+  }
+
+  private isArray(value: T | T[]): value is T[] {
+    return Array.isArray(value);
+  }
+
+  print(index: number = 0): void {
+    if (!this.isArray(this.make) && !this.isArray(this.model)) {
+      this.printPair(this.make, this.model);
+    } else if (this.areBothArrays()) {
+      // Even though we know they're arrays, they need to be asserted as arrays for type-checking as typescript cannot see that in the scope of this function
+      const makeArray = this.make as T[];
+      const modelArray = this.model as T[];
+      // Check if the indices line up and are not out of bounds
+      if (makeArray[index] && modelArray[index]) {
+        this.printPair(makeArray[index], modelArray[index], index);
+      } else {
+        console.log("This NCycle was not created properly.");
+      }
+    } else {
+      console.log("This NCycle was not created properly.");
+    }
+  }
+
+  // Main printAll function
+  printAll(): void {
+    if (this.areArraysMatchingLength()) {
+      // Need to explicitly check if they're arrays to avoid element implicitly being seen as any
+      if (this.isArray(this.make) && this.isArray(this.model)) {
+        for (let i = 0; i < this.make.length; i++) {
+          this.printPair(this.make[i], this.model[i], i);
+        }
+      }
+    } else if (!this.isArray(this.make) && !this.isArray(this.model)) {
+      this.printPair(this.make, this.model);
+    } else {
+      console.log("This NCycle was not created properly.");
+    }
+  }
+}
+
+const singleNCycle = new NCycle("Yamaha", "FZ");
+singleNCycle.print();
+singleNCycle.printAll();
+
+const multiNCycle = new NCycle(["Yamaha", "Honda"], ["FZ", "CBR"]);
+multiNCycle.print();
+multiNCycle.print(1);
+multiNCycle.printAll();
+
+const invalidNCycle = new NCycle(["Yamaha", "Honda"], "CBR");
+invalidNCycle.printAll();
